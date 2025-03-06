b19675d1e88c5aeb7196403485fdad170e564400
TheOneTheOnlyTheChad <chadoberholzer@gmail.com>
2025-03-06 06:43:29 +0200

    Start up files
    Java and such and such

---
+++ README.md
@@ -0,0 +1,3 @@
+# Minecraft: 0:1 Reality Engine
+
+A Minecraft mod that lets players shape the universe using 0:1 Logic, Quantum Mechanics, and Recursive AI.
---
+++ RealityEngineMod.java
@@ -0,0 +1,10 @@
+package com.realityengine;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("realityengine")
+public class RealityEngineMod {
+    public RealityEngineMod() {
+        System.out.println("Welcome to the 0:1 Reality Engine mod!");
+    }
+}
